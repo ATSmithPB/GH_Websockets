6c622d895731f333c4be665fe719830a719383d8
ATSmithPB <97251392+ATSmithPB@users.noreply.github.com>
2022-06-12 18:23:03 +0200

    completed simple local hosted websocket

--- ws/server-echo/server.js
+++ ws/server-echo/server.js
@@ -1,10 +1,31 @@
-
+// Imports
 const WebSocket = require('ws');
 
+// Variables
 const PORT = 5000;
 
 const wsServer = new WebSocket.Server({
     port: PORT
 });
+
+wsServer.on('connection', function (socket){
+    // Some feedback on the console
+    console.log("A client just connected!");
+
+    // Attach some behavior to the incoming socket
+    socket.on('message', function (msg){
+        // Echo input from any client
+        console.log("Received message from client: " + msg);
+        socket.send("Echo: " + msg);
+
+        // Broadcast msg to all connected clients
+        wsServer.clients.forEach(function (client){
+            client.send("Someone said: " + msg);
+        });
+
+    });
+
+});
+
 
 console.log((new Date()) + " Server is listening on port: " + PORT)
